d97dc036b5df9b318dfd2d7fa0af5b8499b3a1b6
AlessandroSaba1991 <sabaalessandro515@gmail.com>
2022-05-24 15:21:04 +0200

    milestone 1 complete

--- index.php
+++ index.php
@@ -19,6 +19,28 @@ require __DIR__ . '/db.php';
             <img src="./assets/img/spotify.png" alt="logo spotify" />
         </div>
     </header>
+    <main class="py_5">
+        <section class="albums py-3">
+            <div class="container">
+                <div class="row gy-4 gx-5 row-cols-1 row-cols-md-2 row-cols-lg-4 row-cols-xxl-5">
+                    <?php foreach ($data['response'] as $disc) : ?>
+                    <div class="col">
+                        <div class="card_album text-center p-4">
+                            <div class="img">
+                                <img src="<?= $disc['poster']?>" alt="<?= $disc['title']?>" />
+                            </div>
+                            <div class="text">
+                                <h4><?= $disc['title']?></h4>
+                                <h5><?= $disc['author']?></h5>
+                                <h6><?= $disc['year']?></h6>
+                            </div>
+                        </div>
+                    </div>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+        </section>
+    </main>
 </body>
 
 </html>
